2efe6e46d3f0106c7cf01ba9941a417aa5d25fb2
Yash Maheshwari <yashmaheshwari696@gmail.com>
2020-09-04 23:05:25 +0530

    assignment 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const style = {
+      color: 'lightblue',     
+  }
+
+  return (
+    <div className="App" style={style}>
+      <h1>Hello, World..!!</h1>
+    </div>
+  );
+}
+
+export default App;
